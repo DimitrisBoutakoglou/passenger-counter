f9644b9771fb998973b10903d8aa560e883abc27
Dimitris <57366051+DimitrisBoutakoglou@users.noreply.github.com>
2025-03-30 15:37:16 +0300

    Changed style a little bit and added a previous entries section. Currenty remembers only one amount.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let count = 0;
-let counterScreen = document.getElementById("counter-screen")
+let counterScreen = document.getElementById("counter-screen");
+let prevEntries = document.getElementById("prev-Entries");
 
 const increment = () => {
     count = count + 1;
     counterScreen.innerText = count;
 }
-const save = () => {
+const saveEntries = () => {
     let save = counterScreen.innerText;
+    prevEntries.innerText = "Previous Entries: " + save;
     console.log(save);
 }
